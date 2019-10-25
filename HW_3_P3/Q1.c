8dc5352693e0046ecdbc9d8073fd3930568c510e
Josh Heri <44132579+jheri-cwru@users.noreply.github.com>
2019-10-25 17:01:37 -0400

    Update Q1.c

--- HW_3_P3/Q1.c
+++ HW_3_P3/Q1.c
@@ -2,10 +2,13 @@
 
 int main()
 {
+    // Define 3 integers
     int a = 3;
     int b = 5;
     int c = 0;
 
+    // Subtract the a right-shifted value of integer a from the product of a and b.
+    // Print the result.
     printf("%d", ((a * b) - (((a>>31)+a)>>1)));
     return 0;
 }
