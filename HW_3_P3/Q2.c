e99a5d5c788dacb13434e63b80883e9610dcaf28
Josh Heri <44132579+jheri-cwru@users.noreply.github.com>
2019-10-27 16:35:46 -0400

    Add files via upload

---
+++ HW_3_P3/Q2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int vars[] = {12,15,221,3,432,54,16,67};
+    
+    int i = 0;
+	int j = 0;
+
+    goto function1;
+    
+    function2:
+
+		if(vars[j] > i)
+		{
+			goto function3;		
+		}
+		
+		i = vars[j];
+		
+    function3:
+
+		j++;
+    	
+	function1:
+    	
+		if(j > 7)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			goto function2;
+		}
+	
+	return 0;
+}
+
+
